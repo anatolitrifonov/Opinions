dcb2475901c6955acbc7d75eb45f66e8e907d386
anatolitrifonov <anatoli@trifonov.com>
2016-10-23 22:16:04 -0700

    initial commit

---
+++ OpinionBlobs/src/OpinionBlobs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpinionBlobs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
